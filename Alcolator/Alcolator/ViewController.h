ba8e7cba97107c7954746555cea6ef201fe6c85a
Hanna Xu <hello@hannaxu.com>
2016-03-25 18:39:12 -0700

    Walked through storyboard

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Hanna Xu on 3/25/16.
+//  Copyright © 2016 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
